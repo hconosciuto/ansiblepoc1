b4cf55390eff0c74928000bea1095cc3775cab63
root <root@ansible.herdom.com>
2018-07-19 11:55:17 -0300

    commit hecho 2018-07-19 11.55.17.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Scripts de Ansible Tower para utilizar en las PoCs de Ansible Tower - Grupo 1
+# Scripts de Ansible Tower para las PoCs - Grupo 1
---
+++ grabo.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "commit hecho `date '+%Y-%m-%d %H.%M.%S'`." 
+git push -u origin master
+#echo "commit commit hecho `date '+%Y-%m-%d %H.%M.%S'`." 
